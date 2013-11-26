3b5aa45b51076fdeb7a0becb8ff7894546dced97
root <root@ubun81.(none)>
2013-11-27 00:36:39 +0800

    modify

--- code/MU/scripts/add-missing.sh
+++
@@ -1,8 +0,0 @@
-cp /usr/share/automake*/config.guess .
-cp /usr/share/automake*/config.sub .
-cp /usr/share/automake*/COPYING .
-cp /usr/share/automake*/depcomp .
-cp /usr/share/automake*/INSTALL .
-cp /usr/share/automake*/install-sh .
-cp /usr/share/automake*/missing .
-cp /usr/share/libtool/config/ltmain.sh .
